3e540c52a1f633570e75a2ec3ba19d90684d7e1a
cuong-mq <cuong.mq010901@gmail.com>
2023-08-07 14:08:58 +0700

    Add Logout

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\CategoryController;
 use PHPUnit\Framework\Attributes\PostCondition;
 
@@ -19,26 +20,23 @@ use PHPUnit\Framework\Attributes\PostCondition;
 |
 */
 
-Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
-Route::get('/category/create', [CategoryController::class, 'create'])->name('category.create');
-Route::post('/category/store', [CategoryController::class, 'store'])->name('category.store');
-Route::get('category/{id}/edit', [CategoryController::class, 'edit'])->name('category.edit');
-Route::put('/category/update/{id}', [CategoryController::class, 'update'])->name('category.update');
-Route::delete('/category/delete/{id}', [CategoryController::class, 'delete'])->name('category.delete');
-
-Route::get('/post/list', [PostController::class, 'index'])->name('post.index');
-Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
-Route::post('/post/store', [PostController::class, 'store'])->name('post.store');
-Route::get('/post/edit/{id}', [PostController::class, 'edit'])->name('post.edit');
-Route::put('/post/update/{id}', [PostController::class, 'update'])->name('post.update');
-Route::delete('/post/delete/{id}', [PostController::class, 'delete'])->name('post.delete');
-
+Route::get('/home', [HomeController::class, 'index'])->name('home.index');
 Route::group(['namespace' => 'App\Http\Controllers'], function () {
-    /**
-     * Home Routes
-     */
     Route::group(['middleware' => ['auth']], function () {
-        Route::get('/home', [HomeController::class, 'index'])->name('home.index');
+        Route::get('/logout', [LogoutController::class, 'perform'])->name('logout.perform');
+        Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
+        Route::get('/category/create', [CategoryController::class, 'create'])->name('category.create');
+        Route::post('/category/store', [CategoryController::class, 'store'])->name('category.store');
+        Route::get('category/{id}/edit', [CategoryController::class, 'edit'])->name('category.edit');
+        Route::put('/category/update/{id}', [CategoryController::class, 'update'])->name('category.update');
+        Route::delete('/category/delete/{id}', [CategoryController::class, 'delete'])->name('category.delete');
+
+        Route::get('/post/list', [PostController::class, 'index'])->name('post.index');
+        Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
+        Route::post('/post/store', [PostController::class, 'store'])->name('post.store');
+        Route::get('/post/edit/{id}', [PostController::class, 'edit'])->name('post.edit');
+        Route::put('/post/update/{id}', [PostController::class, 'update'])->name('post.update');
+        Route::delete('/post/delete/{id}', [PostController::class, 'delete'])->name('post.delete');
     });
 
     Route::group(['middleware' => ['guest']], function () {
